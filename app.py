888746ef885770754b95d40659231ec2dd9158a3
Alex-Ul <it.bumerang@gmail.com>
2019-05-22 12:25:11 +0300

    update application commands for first running

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
+from sqlalchemy import exc
+
 from project import app
+from project.fixtures import make_test_user, make_default_currency
+
+
+@app.cli.command()
+def first_filling():
+    try:
+        make_default_currency()
+    except exc.IntegrityError:
+        app.logger.error("first-filling should run only first running")
+
+
+@app.cli.command()
+def test_user():
+    try:
+        make_test_user()
+    except exc.IntegrityError:
+        app.logger.error("test user already exists, try enter with test:test")
 
 
 if __name__ == '__main__':
